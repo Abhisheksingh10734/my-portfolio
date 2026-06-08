e15d7c3a413e567ba04406870064d71b24d9b009
Abhishek Singh <abhisingh10734@gmail.com>
2026-06-08 18:50:18 +0530

    portfolio setup #2

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Hero } from "./sections/Hero"
+import { Navbar } from "./sections/Navbar"
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-[#0A0F1E]">
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
