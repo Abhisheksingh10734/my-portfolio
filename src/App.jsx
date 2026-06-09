4d654e9da5521be082a186d132076c7cf6bf4053
Abhishek Singh <abhisingh10734@gmail.com>
2026-06-09 12:07:39 +0530

    update styling and add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { About } from "./sections/About"
+import { Contact } from "./sections/Contact"
+import { Footer } from "./sections/Footer"
+import { Github } from "./sections/Github"
 import { Hero } from "./sections/Hero"
 import { Navbar } from "./sections/Navbar"
+import { Projects } from "./sections/Projects"
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Projects />
+      <Github />
+      <Contact />
+      <Footer />
     </div>
   )
 }
